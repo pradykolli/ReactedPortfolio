f9c3d44e19c7d559b4e6943ec30023a34e85c87c
Pradeep Kolli <pradeepkolli3@yahoo.in>
2019-12-18 23:01:54 -0600

    adding resume to the site.

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,9 +1,46 @@
 import React from 'react'
-
+import {Row, Col} from 'reactstrap'
+import logo from '../../Assets/Images/logo2.jpg'
+import "./Resume.css"
 function Resume(){
     return (
         <div>
-            <h1>Resume Yo!!</h1>
+            <Row>
+                {/* Code for the left article part */}
+                <Col md="4" className="leftCol">
+                    <div className="backgroundLayer"></div>
+                    <div className="overlayLayer">
+                        <div className="imageSection">
+                            <div className="imageWrapper">
+                                <img src={logo} alt="Pradeep Kolli" />
+                            </div>
+                            <h1 className="nameTitle">Pradeep Kolli</h1>
+                            <p className="description">Front-End Developer</p>
+                        </div>
+                        <div className="careerGoalSection">
+                            <h3 className="nameTitle">Career Goals</h3>
+                            <p className="description">"Create applications and websites with the user in mind, 
+                            building it with a usable and intuitive user interface experience. 
+                            Continually striving to learn new technologies and look for ways to better myself in this rapidly changing industry."</p>
+                        </div>
+                        <div className="careerGoalSection">
+                            <h3 className="nameTitle">Core Strengths</h3>
+                            <p className="description">
+                                <ul>
+                                    <li>Excellent problem solving and Analytical Skills.</li>
+                                    <li>Strong team player.</li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </Col>
+                {/* Code for the right article part */}
+                <Col md="8"></Col>
+            </Row>
         </div>
     );
 }
