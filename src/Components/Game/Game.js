74819537b9ffd547ff877cbbf9b6294336eada55
Pradeep Kolli <pradeepkolli3@yahoo.in>
2020-01-02 18:00:43 -0700

    added state to the game js

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,33 +1,46 @@
-import React from 'react'
-import { Button, Row, Col } from 'reactstrap'
+import React, { Component } from 'react'
+import { Row, Col } from 'reactstrap'
 import './Game.css'
 import GridBox from './GridBox'
 
-// var generatedNumbers = [];
-// function generateNumber(generatedNumbers){
-//     generatedNumbers.push(Math.round(Math.random() * 10));
-// }
-function Game(){
-    return (
-        <div>
-            {/* <h1>Page under construction</h1>
-             <Progress animated color="success" value="35" /> */}
-             <Row>
-                 <Col md="12" className="text-center mt10">
-                     <Button color="success">Generate Number</Button>{/* {generateNumber()} */}
-                 </Col>
-                 <Col md="12">
-                    {/* {generatedNumbers}
-                    {console.log(generatedNumbers)} */}
-                 </Col>
-                 <Col md="12" className="mt10">
-                     <div className="boxLayout">
-                        <GridBox gridSize="25"></GridBox>
-                     </div>
-                 </Col>
-             </Row>
-        </div>
-    );
+
+
+class Game extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            generatedNumbers : []
+        };
+        this.generateNumber = this.generateNumber.bind(this);
+    } 
+    generateNumber(){
+        this.setState({
+            generatedNumbers: [...this.state.generatedNumbers, Math.round(Math.random() * 10)]
+        })
+    } 
+    render(){
+        return (
+            <div>
+                {/* <h1>Page under construction</h1>
+                <Progress animated color="success" value="35" /> */}
+                <h1>Lets play some Bingo!</h1>
+                <Row>
+                    <Col md="12" className="text-center mt10">
+                        <button type="button"color="success" onClick={this.generateNumber}>Generate Number</button>
+                    </Col>
+                    <Col md="12">
+                        {this.state.generatedNumbers}
+                        {console.log(this.state.generatedNumbers)}
+                    </Col>
+                    <Col md="12" className="mt10">
+                        <div className="boxLayout">
+                            <GridBox gridSize="25"></GridBox>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default Game
