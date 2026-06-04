794b4883b1b1544e3f1c83df3358da830ece862f
pradeepkolli <pradeepkolli3@yahoo.in>
2026-06-04 17:24:51 -0500

    Updates

---
+++ precache-manifest.a36ce144b4b0524348f2b7fc13d4af8b.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa697155641b6b272bfec01afa37750d",
+    "url": "/ReactedPortfolio/index.html"
+  },
+  {
+    "revision": "167ea0fcec4240a80bcc",
+    "url": "/ReactedPortfolio/static/css/2.bba53750.chunk.css"
+  },
+  {
+    "revision": "ee232698a66da328e73a",
+    "url": "/ReactedPortfolio/static/css/main.97f268fd.chunk.css"
+  },
+  {
+    "revision": "167ea0fcec4240a80bcc",
+    "url": "/ReactedPortfolio/static/js/2.8f9a07dd.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/ReactedPortfolio/static/js/2.8f9a07dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee232698a66da328e73a",
+    "url": "/ReactedPortfolio/static/js/main.87eed4ed.chunk.js"
+  },
+  {
+    "revision": "bb0c99c3d8fcedf47b87",
+    "url": "/ReactedPortfolio/static/js/runtime-main.ff106e69.js"
+  },
+  {
+    "revision": "8f67da5c804a5962430462340ab2bb0d",
+    "url": "/ReactedPortfolio/static/media/BeforeTheRain.8f67da5c.ttf"
+  },
+  {
+    "revision": "5ba1d2f5d7b55eb4a29318bd2fb3379a",
+    "url": "/ReactedPortfolio/static/media/Mafakanev.5ba1d2f5.otf"
+  },
+  {
+    "revision": "fe32d30b047837c9d2af053e9f6539f9",
+    "url": "/ReactedPortfolio/static/media/SulphurPoint.fe32d30b.ttf"
+  },
+  {
+    "revision": "6d9df851f4c1b5846e009462b750d33e",
+    "url": "/ReactedPortfolio/static/media/banner.6d9df851.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/ReactedPortfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/ReactedPortfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/ReactedPortfolio/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/ReactedPortfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/ReactedPortfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/ReactedPortfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/ReactedPortfolio/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/ReactedPortfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/ReactedPortfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/ReactedPortfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/ReactedPortfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/ReactedPortfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/ReactedPortfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/ReactedPortfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/ReactedPortfolio/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "39636dd7d7cd4a3a84d9a73a23b30770",
+    "url": "/ReactedPortfolio/static/media/goodTimes.39636dd7.ttf"
+  },
+  {
+    "revision": "8107f8b787a40a84fbf3ee220abd84aa",
+    "url": "/ReactedPortfolio/static/media/indianFont.8107f8b7.TTF"
+  },
+  {
+    "revision": "1118c51950cf4ffeb15d1e527417574a",
+    "url": "/ReactedPortfolio/static/media/logo1.1118c519.jpg"
+  },
+  {
+    "revision": "a951ba3f203bf4e68cb8c1407941f484",
+    "url": "/ReactedPortfolio/static/media/logo4.a951ba3f.jpg"
+  }
+]);
